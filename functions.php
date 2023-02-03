f8d497a0c4f1c1d4407e6cb0d96bd294199fffdd
yassine-diallo <e2148172@cmaisonneuve.qc.ca>
2023-02-03 10:40:55 -0500

    s2c2 ajout menu et wp_enquete

--- functions.php
+++ functions.php
@@ -1,8 +1,24 @@
-<?php 
+<?php
+function ajouter_styles() {
 wp_enqueue_style(
-    'style-princ',
-    get_template_directory_uri() . '/style.css', 
-    array(), 
-    filemtime(get_template_directory() . '/style.css'),
-  );
-?>
+  'style-princ',
+  get_template_directory_uri() . '/style.css',
+  array(),
+  filemtime(get_template_directory() . '/style.css'),
+); 
+}
+add_action( 'wp_enqueue_scripts', 'ajouter_styles' );
+
+/*Enregistrement.................................*/
+
+
+  function enregistrement_nav_menu()
+
+  {
+    register_nav_menus(array(
+      'principal' => 'Menu principal',
+      'footer'  => 'Menu pied de page', 
+    ));
+  }
+  add_action('after_setup_theme', 'enregistrement_nav_menu', 0);
+
