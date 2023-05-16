ebaa73af2dda16c146ae18761517027cd9234825
YassineDiallo11 <e2148172@cmaisonneuve.qc.ca>
2023-05-16 15:50:24 -0400

    Adaptation en tablette et bureau

--- Template-parts/search-4w4.php
+++ Template-parts/search-4w4.php
@@ -10,10 +10,11 @@ if (substr($titre,0,1) == '0'){
 ?>   
     <article class="searchTout">
         <div class="searchTitre">
-    <h2><a href="<?php the_permalink(); ?>"> <?= $titre ?></a></h2>
+    
     </div>
 
-<div class="Description">
+<div class="DescriptionS">
+<h2><a href="<?php the_permalink(); ?>"> <?= $titre ?></a></h2> 
     <p class="texte"><?= wp_trim_words(get_the_excerpt(),80) ?></p>
     </div>
     </article>
